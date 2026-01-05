408e887d821a3fa20927a1bce07fe60e8b9e4639
yousentech <83219678+yousentech@users.noreply.github.com>
2026-01-05 14:42:02 +0300

    update

--- models/account_move.py
+++ models/account_move.py
@@ -21,3 +21,31 @@ class xx_account_move_tax_required(models.Model):
     def _default_allow_modify_product_tax_in_invoice(self):
         return self.user_has_groups('yousentech_invoicing_tax_mgt.group_allow_modify_product_tax_in_invoice')
     # *******************************************************************************************************
+
+    @api.constrains('invoice_line_ids')
+    def check_tax_required(self):
+        for rec in self:
+
+            if rec.type in ('out_invoice','out_refund'):
+                for line in rec.invoice_line_ids:
+                    if line.product_id:
+                        if line.product_id.tax_reqiured and not line.tax_ids:
+                            raise ValidationError(
+                                 "تنبيه .. لا يمكن الاستمرار لوجود ضريبة للمنتج (%s) " % line.product_id.name)
+
+    def post(self):
+        self.check_tax_required()
+        return super(xx_account_move_tax_required, self).post()
+    
+class xx_account_move_line_tax_required(models.Model):
+    _inherit = 'account.move.line'
+
+    l_tax_reqiured = fields.Boolean()
+
+    @api.onchange('product_id')
+    def check_tax_required(self):
+        for rec in self:
+            if rec.product_id:
+                rec.l_tax_reqiured = rec.product_id.tax_reqiured
+
+ 
--- models/product_template.py
+++ models/product_template.py
@@ -9,32 +9,3 @@ class xx_add_fields_in_product(models.Model):
     tax_reqiured = fields.Boolean(string="خاضع للضريبة", default=True)
 
  
-
-class xx_account_move_line_tax_required(models.Model):
-    _inherit = 'account.move.line'
-
-    l_tax_reqiured = fields.Boolean()
-
-    @api.onchange('product_id')
-    def check_tax_required(self):
-        for rec in self:
-            if rec.product_id:
-                rec.l_tax_reqiured = rec.product_id.tax_reqiured
-
-class xx_account_move_tax_required(models.Model):
-    _inherit = 'account.move'
-
-    @api.constrains('invoice_line_ids')
-    def check_tax_required(self):
-        for rec in self:
-
-            if rec.type in ('out_invoice','out_refund'):
-                for line in rec.invoice_line_ids:
-                    if line.product_id:
-                        if line.product_id.tax_reqiured and not line.tax_ids:
-                            raise ValidationError(
-                                 "تنبيه .. لا يمكن الاستمرار لوجود ضريبة للمنتج (%s) " % line.product_id.name)
-
-    def post(self):
-        self.check_tax_required()
-        return super(xx_account_move_tax_required, self).post()
